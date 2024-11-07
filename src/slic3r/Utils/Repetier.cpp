d1d11414baa6fea285f79b1061372357017cb5cc
stxve-cr3d <stefan.weiss1@aol.com>
2024-11-07 08:05:32 +0100

    set timeout to 2000ms

--- src/slic3r/Utils/Repetier.cpp
+++ src/slic3r/Utils/Repetier.cpp
@@ -302,7 +302,6 @@ void Repetier::get_printer_config(const CompletionHandler& handler) const {
     set_auth(http);
 
     http.timeout_max(1000);
-   // http.set_timeout(3000);  // Adjust the timeout value as needed
 
     http.form_add("a", "getPrinterConfig")
         .on_complete([&](std::string body, unsigned status) {
