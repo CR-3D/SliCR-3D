f7b36231ffb38a48bec54dea0fb0be99fd116d07
stxve-cr3d <stefan.weiss1@aol.com>
2024-11-27 12:30:30 +0100

    fixed last commit, missing { } 

--- src/slic3r/GUI/3DBed.cpp
+++ src/slic3r/GUI/3DBed.cpp
@@ -592,7 +592,7 @@ void Bed3D::render_system(GLCanvas3D& canvas, const Transform3d& view_matrix, co
         return;
     }
 
-    if (!bottom)
+    if (!bottom) {
         render_model(view_matrix, projection_matrix);
     }
     
