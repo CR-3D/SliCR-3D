7fa2bb64756fa5d21af48ed270bdcb594b2823e6
Martin Šach <martin.sachin@gmail.com>
2024-12-09 17:58:09 +0100

    Disable SLA autoslicing page
    - Also set instances to unprintable for SLA instead
      of removing them from the model

--- src/slic3r/GUI/Plater.hpp
+++ src/slic3r/GUI/Plater.hpp
@@ -231,6 +231,7 @@ public:
     void reload_print();
     void object_list_changed();
     void generate_thumbnail(ThumbnailData& data, unsigned int w, unsigned int h, const ThumbnailsParams& thumbnail_params, Camera::EType camera_type);
+    void regenerate_thumbnails();
 
     
     //std::vector<size_t> load_files(const std::vector<boost::filesystem::path>& input_files, bool load_model = true, bool load_config = true, bool update_dirs = true, bool imperial_units = false);
