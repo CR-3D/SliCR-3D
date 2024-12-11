ad05f0b5d502dd7ad3b8904024eade26c1833a4d
Martin Šach <martin.sachin@gmail.com>
2024-12-11 17:06:06 +0100

    Make thumbnail regeneration a wxEvent
    Also call the event when a bed texture is fully loaded.

--- src/slic3r/GUI/Plater.hpp
+++ src/slic3r/GUI/Plater.hpp
@@ -67,6 +67,9 @@ namespace UndoRedo {
 
 namespace GUI {
 
+wxDECLARE_EVENT(EVT_SCHEDULE_BACKGROUND_PROCESS, SimpleEvent);
+wxDECLARE_EVENT(EVT_REGENERATE_BED_THUMBNAILS, SimpleEvent);
+
 class MainFrame;
 class ConfigOptionsGroup;
 class ObjectManipulation;
@@ -231,7 +234,6 @@ public:
     void reload_print();
     void object_list_changed();
     void generate_thumbnail(ThumbnailData& data, unsigned int w, unsigned int h, const ThumbnailsParams& thumbnail_params, Camera::EType camera_type);
-    void regenerate_thumbnails();
 
     
     //std::vector<size_t> load_files(const std::vector<boost::filesystem::path>& input_files, bool load_model = true, bool load_config = true, bool update_dirs = true, bool imperial_units = false);
