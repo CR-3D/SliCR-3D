f87bb9cae25290264598e704478784b5f797dbb7
stxve-cr3d <stefan.weiss1@aol.com>
2025-03-24 15:51:35 +0100

    Revereted last commit, MMU just crashes on mac for now. Added if win32

--- src/libslic3r/GCode/GCodeProcessor.cpp
+++ src/libslic3r/GCode/GCodeProcessor.cpp
@@ -4887,6 +4887,10 @@ void GCodeProcessor::store_move_vertex(EMoveType type, bool internal_only)
         internal_only
     );
 
+#if __WIN32__
+   TimeMachine& machine = m_time_processor.machines.front();
+   machine.blocks.back().moves.push_back(m_result.moves.size() - 1);
+#endif
     // stores stop time placeholders for later use
     if (type == EMoveType::Color_change || type == EMoveType::Pause_Print) {
         for (size_t i = 0; i < static_cast<size_t>(PrintEstimatedStatistics::ETimeMode::Count); ++i) {
