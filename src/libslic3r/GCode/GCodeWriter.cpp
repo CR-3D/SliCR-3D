11364f174276f87fa699a284dffece37dc6a8288
stxve-cr3d <stefan.weiss1@aol.com>
2025-04-28 12:57:18 +0200

    use firmware_name and not tool_name for pressure advance

--- src/libslic3r/GCode/GCodeWriter.cpp
+++ src/libslic3r/GCode/GCodeWriter.cpp
@@ -213,8 +213,8 @@ std::string GCodeWriter::set_pressure_advance(double pa) const {
     if (FLAVOR_IS(gcfKlipper)) {
         gcode = std::string("SET_PRESSURE_ADVANCE ADVANCE=") + to_string_nozero(pa, 4);
         if (tool_id >= 0 && !this->config.single_extruder_multi_material.value) {
-            if (this->config.tool_name.size() > tool_id && !this->config.tool_name.get_at(tool_id).empty()) {
-                gcode += std::string(" EXTRUDER=") + this->config.tool_name.get_at(tool_id);
+            if (this->config.firmware_name.size() > tool_id && !this->config.firmware_name.get_at(tool_id).empty()) {
+                gcode += std::string(" EXTRUDER=") + this->config.firmware_name.get_at(tool_id);
             } else {
                 gcode += std::string(" EXTRUDER=extruder") + std::to_string(tool_id);
             }
