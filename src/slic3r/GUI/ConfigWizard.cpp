a110f93ccba226d6b8fc2c3e2350a3ac61b8ac5a
stxve-cr3d <stefan.weiss1@aol.com>
2025-08-04 12:02:18 +0200

    Changed association name from PrusaSlicer to SliCR-3D

--- src/slic3r/GUI/ConfigWizard.cpp
+++ src/slic3r/GUI/ConfigWizard.cpp
@@ -1815,8 +1815,8 @@ PageReloadFromDisk::PageReloadFromDisk(ConfigWizard* parent)
 PageFilesAssociation::PageFilesAssociation(ConfigWizard* parent)
     : ConfigWizardPage(parent, _L("Files association"), _L("Files association"))
 {
-    cb_3mf = new wxCheckBox(this, wxID_ANY, _L("Associate .3mf files to PrusaSlicer"));
-    cb_stl = new wxCheckBox(this, wxID_ANY, _L("Associate .stl files to PrusaSlicer"));
+    cb_3mf = new wxCheckBox(this, wxID_ANY, _L("Associate .3mf files to SliCR-3D"));
+    cb_stl = new wxCheckBox(this, wxID_ANY, _L("Associate .stl files to SliCR-3D"));
 //    cb_gcode = new wxCheckBox(this, wxID_ANY, _L("Associate .gcode files to PrusaSlicer G-code Viewer"));
 
     append(cb_3mf);
