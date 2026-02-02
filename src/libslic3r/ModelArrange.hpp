cc9bd00191006f8011f28d3db25f8fd197928fa7
stxve-cr3d <stefan.weiss@cr-3d.de>
2026-02-02 15:45:48 +0100

    Moved Arranging logic into its own folder and wrapper, also fixes some weird issue where it did not calculate the middle of Y correctly.

--- src/libslic3r/ModelArrange.hpp
+++ src/libslic3r/ModelArrange.hpp
@@ -5,30 +5,8 @@
 #ifndef MODELARRANGE_HPP
 #define MODELARRANGE_HPP
 
-#include <libslic3r/Arrange/Scene.hpp>
 
 namespace Slic3r {
-
-class Model;
-class ModelInstance;
-using ModelInstancePtrs = std::vector<ModelInstance*>;
-
-//void duplicate(Model &model, ArrangePolygons &copies, VirtualBedFn);
-void duplicate_objects(Model &model, size_t copies_num);
-
-bool arrange_objects(Model &model,
-                     const arr2::ArrangeBed &bed,
-                     const arr2::ArrangeSettingsView &settings);
-
-void duplicate_objects(Model &              model,
-                       size_t               copies_num,
-                       const arr2::ArrangeBed &bed,
-                       const arr2::ArrangeSettingsView &settings);
-
-void duplicate(Model &              model,
-               size_t               copies_num,
-               const arr2::ArrangeBed &bed,
-               const arr2::ArrangeSettingsView &settings);
 
 } // namespace Slic3r
 
--- src/slic3r/GUI/Jobs/ArrangeJob2.cpp
+++ src/slic3r/GUI/Jobs/ArrangeJob2.cpp
@@ -10,6 +10,7 @@
 #include <libslic3r/Model.hpp>
 #include <libslic3r/TriangleMeshSlicer.hpp>
 #include <libslic3r/Geometry/ConvexHull.hpp>
+#include <libslic3r/MultipleBeds.hpp>
 
 #include <libslic3r/SLAPrint.hpp>
 #include <libslic3r/Print.hpp>
@@ -72,7 +73,6 @@ public:
         return ret;
     }
 };
-
 
 static Polygon get_wtpoly(const GLCanvas3D::WipeTowerInfo &wti)
 {
@@ -191,7 +191,9 @@ arr2::SceneBuilder build_scene(Plater &plater, ArrangeSelectionMode mode)
                 const bool is_selected{gui_selection->selected_instances(object_index)[instance_index]};
                 const auto instance_bed_index{beds_map.find(instance->id())};
 
-                if (is_selected || instance_bed_index != beds_map.end()
+                if (
+                    is_selected
+                    || instance_bed_index != beds_map.end()
                 ) {
                     considered_instances.insert(instance->id());
                 }
@@ -212,7 +214,13 @@ arr2::SceneBuilder build_scene(Plater &plater, ArrangeSelectionMode mode)
                 const bool is_selected{gui_selection->selected_instances(object_index)[instance_index]};
 
                 const auto instance_bed_index{beds_map.find(instance->id())};
-                if (is_selected || ( instance_bed_index != beds_map.end() && instance_bed_index->second == current_bed)) {
+                if (
+                    is_selected
+                    || (
+                        instance_bed_index != beds_map.end()
+                        && instance_bed_index->second == current_bed
+                    )
+                ) {
                     constraints.insert({instance->id(), current_bed});
                     considered_instances.insert(instance->id());
                 }
